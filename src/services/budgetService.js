49f8e7dae81ab6d0c3d126f83357c4a284e9ede1
nhalkogit config --global init.defaultBranch main <nata26564@gmail.com>
2025-04-14 19:12:48 -0500

    updated budget and headers

--- src/services/budgetService.js
+++ src/services/budgetService.js
@@ -3,7 +3,7 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/budgets`;
 const index = async () => {
     try {
     const res = await fetch(BASE_URL, {
-      headers: getHeaders(),
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
     });
     return await res.json();
  } catch (err) {
@@ -15,8 +15,8 @@ const index = async () => {
 const show = async (budgetId) => {
     try {
         const res = await fetch (`${BASE_URL}/${budgetId}`, {
-            headers: getHeaders(),
-          });
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+        });
     return await res.json();
 } catch (err) {
        console.log(err);
