a7291c21e99cabb581689a65f28f1bc7272b3288
Zebastian Rodriguez <Zebo5914@gmail.com>
2025-04-15 20:07:22 -0400

    budget form renders in dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,35 +1,41 @@
-import { useEffect, useState, } from "react";
+import { useEffect, useState } from "react";
 
-import * as budgetService from '../../services/budgetService';
+import * as budgetService from "../../services/budgetService";
+import BudgetForm from "../BudgetForm/BudgetForm";
 
 const Dashboard = () => {
-    const [ budgets, setBudgets ] = useState([]);
+	const [budgets, setBudgets] = useState([]);
 
-    useEffect(() => {
-        const fetchBudgets = async () => {
-            try {
-                const fetchedBudgets = await budgetService.index();
-                setBudgets(fetchedBudgets);
-            } catch (err) {
-                console.log(err)
-            }
-        }
-        if (budgets) fetchBudgets();
-    }, [budgets]);
+	useEffect(() => {
+		const fetchBudgets = async () => {
+			try {
+				const fetchedBudgets = await budgetService.index();
+				setBudgets(fetchedBudgets);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		if (budgets) fetchBudgets();
+	}, [budgets]);
 
-
-    return (
-        <main>
-            {budgets ? (
-                <ul>
-                {budgets.map(budget => (
-                    <li key={budget._id}>{budget.name}</li>
-                ))}
-            </ul>
-            ) : <p>This is the dashboard where you can see all of your budgets!</p> 
-            }            
-        </main>
-    )
+	return (
+		<div>
+			<main>
+				<BudgetForm />
+			</main>
+			<section>
+				{budgets ? (
+					<ul>
+						{budgets.map((budget) => (
+							<li key={budget._id}>{budget.name}</li>
+						))}
+					</ul>
+				) : (
+					<p>This is the dashboard where you can see all of your budgets!</p>
+				)}
+			</section>
+		</div>
+	);
 };
 
 export default Dashboard;
