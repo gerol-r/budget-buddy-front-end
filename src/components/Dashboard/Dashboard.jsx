17e6eff9480a8415fa7f2b568a4389e5ae75aa5b
gerol-r <gerol.revills@gmail.com>
2025-04-21 17:27:59 -0400

    final reformat

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,14 +2,13 @@ import { useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import WelcomeBuddy from "../../Images/Welcome-buddy.png";
 
-
 const Dashboard = () => {
   const { user } = useContext(UserContext);
 
   return (
     <main>
       <h1>
-        Welcome to the Home Page {user.username}{" "}
+        Welcome! {user.username}{" "}
         <img
           src={user?.avatar}
           alt="User Avatar"
@@ -17,25 +16,11 @@ const Dashboard = () => {
         />
         !
       </h1>
+      <div>
+        <img src={WelcomeBuddy} alt="logo" className="welcome-image" />
+      </div>
     </main>
   );
 };
 
-
-    return (
-
-        <main>
-            <h1>Welcome! {user.username} <img
-              src={user?.avatar}
-              alt="User Avatar"
-              style={{ width: "50px", borderRadius: "50%" }}
-            />!</h1>
-            <div>
-                <img src={WelcomeBuddy} alt="logo"  className="welcome-image"/>
-            </div>
-        </main>
-    )
-}
-
 export default Dashboard;
-
